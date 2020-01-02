b02a4aedebfec421210625fdc2fa71fbc927d49f
Zhuanle <1847496393@qq.com>
2020-01-02 15:40:16 +0800

    ordinary2

--- src/com/zhl/Main.java
+++ src/com/zhl/Main.java
@@ -7,6 +7,7 @@ public class Main{
     System.out.println(removeDuplicates(nums));
   }
 
+  //去除重复数字
   public static int removeDuplicates(int[] nums) {
     // write your code here
     int size = nums.length;
